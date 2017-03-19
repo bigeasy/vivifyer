09084b2e584a8dff6908396e1d2b1562b4e24dfc
Alan Gutierrez <alan@prettyrobots.com>
2017-03-19 14:06:46 -0500

    Implementation.

--- t/vivifyer.t.js
+++ t/vivifyer.t.js
@@ -1,6 +1,15 @@
-require('proof/redux')(1, prove)
+require('proof/redux')(6, prove)
 
 function prove (assert) {
     var Vivifyer = require('..')
+    var counter = 0
+    var vivifyer = new Vivifyer(function (key) {
+        assert(key == 'x', 'constructor')
+        return counter++
+    })
     assert(Vivifyer, 'required')
+    assert(vivifyer.get('x'), 0, 'get')
+    assert(vivifyer.get('x'), 0, 'get exists')
+    assert(vivifyer.remove('x'), 0, 'remove')
+    assert(!('x' in vivifyer.map), 'map')
 }
--- vivifyer.js
+++ vivifyer.js
@@ -1,1 +1,23 @@
+function Vivifyer (constructor) {
+    this._constructor = constructor
+    this.map = {}
+}
 
+Vivifyer.prototype._vivify = function (key) {
+    if (!(key in this.map)) {
+        return this.map[key] = this._constructor.call(null, key)
+    }
+    return this.map[key]
+}
+
+Vivifyer.prototype.get = function (key) {
+    return this._vivify(key)
+}
+
+Vivifyer.prototype.remove = function (key) {
+    var value = this.map[key]
+    delete this.map[key]
+    return value
+}
+
+module.exports = Vivifyer
